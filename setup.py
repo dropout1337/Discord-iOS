8cd0074b8cff6b5b29545c3217c56b31a7ec876f
dropout <dropout@fbi.ac>
2022-06-04 19:29:14 +1200

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="discord_ios",
+    version="1.0.0",
+    packages=find_packages(),
+    include_package_data=True,
+    license="MIT License",
+    description="Make your discord.py bots appear as if they are on a phone.",
+    keywords="discord.py, discord ios, discord ios status, discord bot mobile status, discord mobile bot status",
+    url="https://github.com/dropout1337/Discord-iOS",
+    author="dropout1337",
+    author_email="dropout@fbi.ac",
+    classifiers=[
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+    ]
+)
